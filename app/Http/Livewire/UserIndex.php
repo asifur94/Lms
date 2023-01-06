997f9ad856bbcbabd5df976a3a703b724641ba7e
Md Asifur Rahman <asifur94@gmail.com>
2023-01-07 00:13:54 +0600

    user

--- app/Http/Livewire/UserIndex.php
+++ app/Http/Livewire/UserIndex.php
@@ -8,11 +8,26 @@ use Livewire\Component;
 
 class UserIndex extends Component
 {
+    // public function render()
+    // {
+    //     $users = User::where('name', '!=', 'Super Admin')->get();
+    //     return view('livewire.user-index',  [
+    //         'users' => $users
+    //     ]);
+
+    //     User::with('roles')->paginate();
+    //}
     public function render()
     {
-        $users = User::all();
-        return view('livewire.user-index',  [
-            'users' => $users
-        ]);
+        $users = User::with('roles')->paginate(10);
+        return view('livewire.user-index',['users' => $users]);
+    }
+    public function userDelete($id)
+    {
+        $user = User::findOrFail($id);
+
+        $user->delete();
+        flash()->addSuccess('User deleted successfully!');
+        return redirect()->route('user.index');
     }
 }
